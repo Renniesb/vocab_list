d00b1ad6efd9532f35ad4e24b7990b42280b58aa
Rennie Bevineau <rsbevineau@gmail.com>
2015-11-10 18:51:59 -0700

    resolve logging of all results

--- scripts/controllers/results-controller.js
+++ scripts/controllers/results-controller.js
@@ -1,7 +1,18 @@
 vocabApp.controller('resultsController', ['$scope','$resource', 'wordService', function  ($scope, $resource, wordService) {
 	$scope.word = wordService.word;
 
-	$scope.dictionaryAPI = $resource("https://glosbe.com/gapi_v0_1/translate?from=es&dest=es&format=json&phrase=hola&tm=true&pretty=true");
-	$scope.dictionaryResults = $scope.dictionaryAPI.get();
+	$scope.dictionaryAPI = $resource("https://glosbe.com/gapi/translate?from=es&dest=es&format=json&phrase="+ $scope.word.toLowerCase()+"&tm=true&pretty=true", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+	$scope.dictionaryResults = $scope.dictionaryAPI.get().$promise.then(function(data){
+
+            	var word = "";
+
+            	for (i = 0; i < data.tuc.length; i++) {
+            		if(data.tuc[i].meanings)
+    				{word = data.tuc[i]}
+
+				}
+
+                console.log(word.meanings);
+            });
 
 }]);
