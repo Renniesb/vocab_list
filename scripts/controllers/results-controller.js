f6f3ff4beadea33137aef75248b40b8afc75546b
Rennie Bevineau <rsbevineau@gmail.com>
2015-11-10 18:53:16 -0700

    refactor json search code

--- scripts/controllers/results-controller.js
+++ scripts/controllers/results-controller.js
@@ -4,7 +4,7 @@ vocabApp.controller('resultsController', ['$scope','$resource', 'wordService', f
 	$scope.dictionaryAPI = $resource("https://glosbe.com/gapi/translate?from=es&dest=es&format=json&phrase="+ $scope.word.toLowerCase()+"&tm=true&pretty=true", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
 	$scope.dictionaryResults = $scope.dictionaryAPI.get().$promise.then(function(data){
 
-            	var word = "";
+            	var word;
 
             	for (i = 0; i < data.tuc.length; i++) {
             		if(data.tuc[i].meanings)
