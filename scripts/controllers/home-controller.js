126743fac39d7b5ed2e7579189be0c899e3113a1
Rennie <rsbevineau@gmail.com>
2015-12-20 02:41:03 -0700

    add photo and instructions

--- scripts/controllers/home-controller.js
+++ scripts/controllers/home-controller.js
@@ -1,9 +1,11 @@
-vocabApp.controller('homeController', ['$scope', 'wordService', '$location', function ($scope, wordService, $location) {
+vocabApp.controller('homeController', ['$scope', 'wordService', '$location','$sce', function ($scope, wordService, $location, $sce) {
 		$scope.word = wordService.word;
 		// $(".wordsearch").addClass(".active");
 		$scope.$watch('word', function  () {
 			wordService.word = $scope.word;
 		} );
+
+		$scope.myImgSrc = $sce.trustAsResourceUrl('/vocab_list/images/bandera.jpg');
 
 		$scope.submit = function () {
 			$location.path("/results");
